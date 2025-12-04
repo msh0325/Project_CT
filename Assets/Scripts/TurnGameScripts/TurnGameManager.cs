ae55c361f4bf52d14d3e244ca79ed3591004453a
msh0325 <113653680+msh0325@users.noreply.github.com>
2025-12-04 10:00:01 +0900

    Edit TurnGameManager
    나중에 추가할 부분 주석으로 써둠

--- Assets/Scripts/TurnGameScripts/TurnGameManager.cs
+++ Assets/Scripts/TurnGameScripts/TurnGameManager.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public class TurnGameManager : MonoBehaviour
 {
     [Header("Data")]
-    public CSVReader cSVReader;
+    public CSVReader csvReader;
+    public TextAsset characterCSV;
+    public TextAsset skillCSV;
 
     [Header("파티 구성")]
     public List<string> allyCharacterIDs;
@@ -19,8 +21,19 @@ public class TurnGameManager : MonoBehaviour
 
     void Start()
     {
-        cSVReader.readData = ReadData.CharacterData;
-        cSVReader.ReadCSV();
+        // 나중에 세부 구현할 때 데이터는 따로 매니저 빼서 관리하는게 좋을듯
+        // 씬 전환되면서 데이터는 어케 옮김?
+        // >> 캐릭터&스킬데이터는 DataManager(싱글톤 & dontdestroyonload), 유동적인 정보(즉, 캐릭터 성장, 편성 등)은 PlayerData(싱글톤 & dontdestroyonload)
+
+        // characterData 불러오기
+        csvReader.csvFile = characterCSV;
+        csvReader.readData = ReadData.CharacterData;
+        csvReader.ReadCSV();
+
+        // skillData 불러오기
+        csvReader.csvFile = skillCSV;
+        csvReader.readData = ReadData.SkillData;
+        csvReader.ReadCSV();
     }
 
     void Update()
